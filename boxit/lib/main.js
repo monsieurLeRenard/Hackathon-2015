0079264ad60adc5090f28c5200a7ef8bc2cb7664
monsieurLeRenard <daniele.tabanella@photobox.com>
2015-06-26 10:05:06 +0200

    plugin

---
+++ boxit/lib/main.js
@@ -0,0 +1,47 @@
+
+var buttons = require("sdk/ui/button/toggle");
+var tabs 	= require("sdk/tabs");
+var data 	= require("sdk/self").data;
+var pageMod = require("sdk/page-mod");
+
+var data 	= require("sdk/self").data;
+var pageMod = require("sdk/page-mod");
+
+var button = buttons.ToggleButton({
+  id: "photobox-toolbar",
+  label: "Photobox toolbar",
+  icon: {
+    "16": "./icon-16.png",
+    "32": "./icon-32.png",
+    "64": "./icon-64.png"
+  },
+  onChange: changed,
+  badge: 'on',
+  badgeColor: "#00AAAA"
+ });
+
+ var toolbarLaunch = false;
+
+function changed(state) {
+  if (state.checked) {
+  button.badge = 'off';
+    button.badgeColor = "#F43D42";
+    toolbarLaunch = true;
+  }
+  else {
+  button.badge = 'on';
+    button.badgeColor = "#44E443";
+    toolbarLaunch = false;
+  }
+}
+
+pageMod.PageMod({
+	include: '*',
+	attachTo: 'top',
+	contentScriptFile: [data.url("jquery.1.11.2.min.js"), data.url("toolbar.js")],
+	onAttach: function onAttach(worker) {
+		worker.on('message', function(newabc) {
+		  abc = newabc;
+		});
+	}
+});
